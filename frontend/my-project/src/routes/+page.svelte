ffbf1c7306772d25eaa330d1ca149b59686fa441
Kreditserve <hrutik0729@gmail.com>
2025-02-21 15:20:47 +0530

    all done

--- frontend/my-project/src/routes/+page.svelte
+++ frontend/my-project/src/routes/+page.svelte
@@ -3,8 +3,9 @@
     import { onMount } from "svelte";
 
 
-
+let showModal=false
 let data=[]
+let updatedBookData={ title:"" , author:"" , published_date:"" , price:"" , id:""}
 const fetchdata=()=>{
     axios.get("http://127.0.0.1:8000/api/books")
     .then(res=>
@@ -16,6 +17,19 @@ const fetchdata=()=>{
     )
 }
 
+const update=(book)=>{
+   updatedBookData={...book}
+   showModal=true
+}
+const updatedata=(id)=>{
+    axios.patch(`http://127.0.0.1:8000/api/books/${id}`,updatedBookData)
+    .then(res=>{ console.log(res.data)})
+.catch(err=>
+console.log(err)
+)
+      
+
+}
 const handeldelete=(id)=>{
     axios.delete(`http://127.0.0.1:8000/api/books/${id} `)
     .then(res=>
@@ -52,7 +66,7 @@ onMount(()=>{
                 <td class="border-1 p-2">{item.published_date}</td>
                 <td class="border-1 p-2">
                     
-                    <button class="p-1 pl-3 pr-3 bg-green-500 text-amber-50 m-1 ml-2 mr-2 rounded-xl">Update</button>
+                    <button class="p-1 pl-3 pr-3 bg-green-500 text-amber-50 m-1 ml-2 mr-2 rounded-xl" on:click={()=>update(item)}>Update</button>
                     <button class="p-1 pl-3 pr-3 bg-red-500 text-amber-50 m-1 ml-2 mr-2 rounded-xl" on:click={()=>handeldelete(item.id)}>Delete</button>
 
                 </td>
@@ -60,5 +74,31 @@ onMount(()=>{
             {/each}
         </tbody>
     </table>
+    {#if showModal}
+    <div class="fixed inset-0 flex items-center justify-center bg-opacity-30 backdrop-blur-md">
+        <div class="bg-white p-6 rounded-lg shadow-lg w-1/3">
+          <div class="w-full p-3 text-center">
+              <h1 class="text-3xl font-bold">Update Book Information</h1>
+          </div>
+          <div class="w-12/12 m-auto mt-5 p-1 justify-center">
+              
+              <label>Book Name</label>
+              <input placeholder="book name" type="text" bind:value={updatedBookData.title} class="w-full border-b-2 p-0.5 pl-2 mb-2 mt-2 outline-0 focus:border-red-600"/>
+  
+              <label>Author Name</label>
+              <input placeholder="author name" type="text" bind:value={updatedBookData.author} class="w-full border-b-2 p-0.5 pl-2 mb-2 mt-2 outline-0 focus:border-red-600"/>
+  
+              <label>Published Date</label>
+              <input placeholder="book published_date" type="date" bind:value={updatedBookData.published_date} class="w-full border-b-2 p-0.5 pl-2 mb-2 mt-2 outline-0 focus:border-red-600"/>
+  
+              <label>Price</label>
+              <input placeholder="book price" type="number" bind:value={updatedBookData.price} class="w-full border-b-2 p-0.5 pl-2 mb-2 mt-2 outline-0 focus:border-red-600"/>
+  
+              <button class="w-full mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl" on:click={()=>updatedata(updatedBookData.id)}>Update</button>
+          </div>
+        </div>
+  </div>
+  
+  {/if}
 
 </div>
