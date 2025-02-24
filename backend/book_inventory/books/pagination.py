b55cc54bb262235693f89ad75faa83cda2f2ccd2
Yadla Uday Kumar <udaykumar3439@gmail.com>
2025-02-24 18:47:56 +0530

    modified response in pagination results

--- backend/book_inventory/books/pagination.py
+++ backend/book_inventory/books/pagination.py
@@ -1,7 +1,21 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
 
 class BookPagination(PageNumberPagination):
-    page_size=5 #Set number of records per page
-    page_size_query_param='limit' #allow users to change records per page
-    max_page_size=59 #prevents users from requesting too many records
-    
+    page_size = 5  # Set number of records per page
+    page_size_query_param = "page_size"  # allow users to change records per page
+    max_page_size = 100  # prevents users from requesting too many records
+
+    def get_paginated_response(self, data):
+        return Response(
+            {
+                "total_pages": self.page.paginator.num_pages,
+                "page_size": self.page.paginator.per_page,
+                "current_page": self.page.number,
+                "total_items": self.page.paginator.count,  # total records in db
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link(),
+                "results": data,  # the actual paginated data
+            }
+        )
