eea97978f4317e7585dbfd504f881990e8313a27
Yadla Uday Kumar <udaykumar3439@gmail.com>
2025-02-24 15:15:33 +0530

    pagination feature is added

--- backend/book_inventory/books/views.py
+++ backend/book_inventory/books/views.py
@@ -5,11 +5,14 @@ from django.shortcuts import render
 from rest_framework import viewsets,filters
 from .models import Book
 from .serializers import BookSerializer
+from .pagination import BookPagination
 from django_filters.rest_framework import DjangoFilterBackend
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    pagination_class=BookPagination #apply custom pagination
+    
 
     # Add filtering and search capabilities
     filter_backends=[filters.SearchFilter,DjangoFilterBackend]
