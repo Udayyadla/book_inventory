43d7c19d7c74add71aacf00ec3f90275df5e0ab4
Yadla Uday Kumar <udaykumar3439@gmail.com>
2025-02-24 17:00:59 +0530

    filter and sorting is added

--- backend/book_inventory/book_inventory/settings.py
+++ backend/book_inventory/book_inventory/settings.py
@@ -138,6 +138,10 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.OrderingFilter",
+    ],
     "DEFAULT_RENDERER_CLASSES": (
         "rest_framework.renderers.JSONRenderer",  # Force JSON response
     ),
